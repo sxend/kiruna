fb8690f88593230206a1ec118fbf6ad01053dc8c
sxend <arimitsu.k+github@gmail.com>
2017-09-28 18:30:37 +0900

    add actor sample

--- examples/actor_sample.rs
+++ examples/actor_sample.rs
@@ -1,17 +1,33 @@
 extern crate kiruna;
 
 use kiruna::prelude::*;
+use std::sync::Arc;
+use std::any::Any;
+use std::fmt::Display;
 
 fn main() {
     let actor_system = ActorSystem::new("name");
-    let actor_ref = actor_system.actor_of(Props::new(|| SampleActor));
-    actor_ref.send("hello".to_string());
+    let props = Props::new(Arc::new(|| SampleActor));
+    let actor_ref = actor_system.actor_of(props);
+    actor_ref.send(Box::new(SampleMessage("hello".to_string())));
 }
 
 struct SampleActor;
 
 impl Actor for SampleActor {
-    fn receive(&self, msg: String) {
-        println!("{}", msg);
+    fn receive(&self, msg: Box<Any>) {
+        if let Ok(message) = Box::<Any>::downcast::<SampleMessage>(msg) {
+            println!("{}", *message);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct SampleMessage(String);
+unsafe impl Send for SampleMessage {}
+unsafe impl Sync for SampleMessage {}
+impl Display for SampleMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Debug::fmt(self, f)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,68 @@
 
 //pub type String = Send + Sync;
+use std::any::Any;
+use std::sync::Arc;
 
 pub struct ActorSystem {
-
+    name: String
 }
 
 impl ActorSystem {
     pub fn new(name: &str) -> ActorSystem {
-        ActorSystem {}
+        ActorSystem {
+            name: name.to_string()
+        }
     }
-    pub fn actor_of<A: 'static + Actor, F: Fn() -> A>(&self, props: Props<A, F>) -> Box<ActorRef> {
-        let local = LocalActorRef::new(props.new_actor());
-        Box::new(local)
-    }
-}
-
-pub trait ActorRef {
-    fn send(&self, message: String);
-}
-
-struct LocalActorRef<A: Actor> {
-    internal: A
-}
-impl<A: Actor> LocalActorRef<A> {
-    fn new(internal: A) -> LocalActorRef<A> {
-        LocalActorRef {
-            internal: internal
+    pub fn actor_of(&self, props: Arc<ActorFactory>) -> ActorRef {
+        ActorRef {
+            inner: props.clone().create()
         }
     }
 }
-impl<'a, A: Actor> ActorRef for LocalActorRef<A> {
-    fn send(&self, message: String) {
-        self.internal.receive(message);
+
+pub struct ActorRef {
+    inner: Arc<Actor>,
+}
+
+impl ActorRef {
+    pub fn send(&self, message: Box<Any>) {
+        self.inner.receive(message)
     }
 }
 
-pub trait Actor {
-    fn receive(&self, message: String);
+pub trait Actor: Send + Sync + 'static {
+    fn receive(&self, message: Box<Any>);
 }
 
-pub struct Props<A: Actor, F: Fn() -> A> {
-    factory: F,
+pub struct Props<A: Actor> {
+    factory: Arc<Fn() -> A + Sync + Send>
 }
 
-impl<A: Actor, F: Fn() -> A> Props<A, F> {
 
-    pub fn new(factory: F) -> Props<A, F> {
-        Props {
+impl<A: Actor> Props<A> {
+
+    pub fn new(factory: Arc<Fn() -> A + Sync + Send>) -> Arc<ActorFactory> {
+        Arc::new(Props {
             factory: factory,
-        }
+        })
     }
-    fn new_actor(&self) -> A {
-        let factory = &self.factory;
-        factory()
+    fn new_actor(&self) -> Arc<Actor> {
+        let f = self.factory.clone();
+        Arc::new(f())
     }
 }
+pub trait ActorFactory: Send + Sync {
+    fn create(&self) -> Arc<Actor>;
+}
 
-trait ActorFactory<A: Actor> {
-    fn create(&self) -> A;
+impl<A: Actor> ActorFactory for Props<A> {
+    fn create(&self) -> Arc<Actor> {
+        self.new_actor()
+    }
 }
 
 pub mod prelude {
-    pub use {ActorSystem, Actor, Props};
+    pub use {ActorSystem, Actor, Props, ActorFactory};
 }
 
 #[cfg(test)]
