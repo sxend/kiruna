df353339b73fc6cf7d22de47ee0addc51c61f790
sxend <arimitsu.k+github@gmail.com>
2017-09-29 15:55:29 +0900

    fix interface

--- tests/actor.rs
+++ tests/actor.rs
@@ -1,18 +1,20 @@
 extern crate kiruna;
 
+use std::sync::Arc;
+use std::any::Any;
 use kiruna::prelude::*;
 
 #[test]
 fn actor_system() {
-    let actor_system = ActorSystem::new("name");
-    let actor_ref = actor_system.actor_of(Props::new(|| SampleActor));
-    actor_ref.send("hello".to_string());
+    let actor_system = ActorSystem::new("name".to_string());
+    let actor_ref = actor_system.actor_of(Props::new(Arc::new(|| SampleActor)));
+    actor_ref.send(Box::new("hello".to_string()));
 }
 
 struct SampleActor;
 
 impl Actor for SampleActor {
-    fn receive(&self, msg: String) {
+    fn receive(&self, sender: Arc<ActorRef>, context: Arc<ActorContext>, msg: Box<Any>) {
         println!("{:?}", msg);
     }
 }
