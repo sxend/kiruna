02ec2f596b1fa580e47b51ccdb0a5a716c3bb0f3
sxend <arimitsu.k+github@gmail.com>
2017-10-02 22:10:54 +0900

    rename pool

--- src/actor_ref.rs
+++ src/actor_ref.rs
@@ -11,42 +11,53 @@ use self::jobpool::JobPool;
 
 pub struct ActorRef {
     inner: Arc<InnerActorRef>,
-    pool: Arc<Mutex<JobPool>>,
 }
 
 impl ActorRef {
-    pub fn new(underlying: Arc<Actor>, pool: Arc<Mutex<JobPool>>) -> ActorRef {
+    pub fn new(underlying: Arc<Actor>, dispatcher: Arc<Mutex<JobPool>>) -> ActorRef {
         ActorRef {
-            inner: Arc::new(InnerActorRef {
-                underlying: underlying,
-                mailbox: Arc::new(Mutex::new(vec![])),
-            }),
-            pool
+            inner: Arc::new(InnerActorRef::new(underlying, dispatcher)),
+        }
+    }
+    pub fn send<M: Message>(&self, message: M) {
+        self.inner.send(message)
+    }
+    pub fn ask<M: Message>(&self, message: M) -> Receiver<Box<Any + Send + Sync>> {
+        self.inner.ask(message)
+    }
+}
+
+struct InnerActorRef {
+    underlying: Arc<Actor>,
+    mailbox: Arc<Mutex<Vec<(Box<Any + Send + Sync>, Sender<Box<Any + Send + Sync>>)>>>,
+    dispatcher: Arc<Mutex<JobPool>>,
+}
+impl InnerActorRef {
+    fn new(underlying: Arc<Actor>,
+           dispatcher: Arc<Mutex<JobPool>>) -> InnerActorRef {
+        InnerActorRef {
+            underlying: underlying,
+            mailbox: Arc::new(Mutex::new(vec![])),
+            dispatcher: dispatcher
         }
     }
     pub fn send<M: Message>(&self, message: M) {
         self.ask(message);
     }
     pub fn ask<M: Message>(&self, message: M) -> Receiver<Box<Any + Send + Sync>> {
-        let mailbox = self.inner.mailbox.clone();
+        let mailbox = self.mailbox.clone();
         let mailbox = mailbox.lock();
-        mailbox.unwrap().push(Box::new(message));
-        let inner = self.inner.clone();
         let (tx, rx): (Sender<Box<Any + Send + Sync>>, Receiver<Box<Any + Send + Sync>>) = channel();
-        let tx = tx.clone();
-        let pool = self.pool.clone();
-        let pool = pool.lock();
-        pool.unwrap().queue(move || {
-            let mailbox = inner.mailbox.clone();
-            let message = mailbox.lock().unwrap().pop().unwrap();
-            let underlying = inner.underlying.clone();
+        mailbox.unwrap().push((Box::new(message), tx.clone()));
+
+        let dispatcher = self.dispatcher.clone();
+        let dispatcher = dispatcher.lock();
+        let mailbox = self.mailbox.clone();
+        let underlying = self.underlying.clone();
+        dispatcher.unwrap().queue(move || {
+            let (message, tx) = mailbox.lock().unwrap().pop().unwrap();
             underlying.receive(tx, Arc::new(ActorContext), message);
         });
         rx
     }
-}
-
-struct InnerActorRef {
-    underlying: Arc<Actor>,
-    mailbox: Arc<Mutex<Vec<Box<Any + Send + Sync>>>>
 }
--- src/actor_system.rs
+++ src/actor_system.rs
@@ -8,27 +8,35 @@ use self::jobpool::JobPool;
 
 pub struct ActorSystem {
     name: String,
-    actor_refs: Arc<Mutex<Vec<ActorRef>>>,
-    pool: Arc<Mutex<JobPool>>
+    inner: Arc<InnerActorSystem>,
 }
 
 impl ActorSystem {
     pub fn new(name: String) -> ActorSystem {
         ActorSystem {
-            name: name,
-            actor_refs: Arc::new(Mutex::new(vec![])),
-            pool: Arc::new(Mutex::new(JobPool::new(4)))
+            name: name.to_owned(),
+            inner: Arc::new(InnerActorSystem::new(name.to_owned())),
         }
     }
     pub fn actor_of(&self, props: Arc<ActorFactory>, name: String) -> ActorRef {
-        let actor_ref = ActorRef::new(props.clone().create(), self.pool.clone());
-//        let actor_refs = self.actor_refs.clone();
-//        let actor_refs = actor_refs.lock();
-//        let mut actor_refs = actor_refs.unwrap();
-//        actor_refs.push(actor_ref);
-        actor_ref
+        self.inner.actor_of(props, name)
     }
-    pub fn get_name(&self) -> String {
-        self.name.to_owned()
+}
+
+struct InnerActorSystem {
+    name: String,
+    dispatcher: Arc<Mutex<JobPool>>
+}
+
+impl InnerActorSystem {
+    fn new(name: String) -> InnerActorSystem {
+        InnerActorSystem {
+            name,
+            dispatcher: Arc::new(Mutex::new(JobPool::new(4)))
+        }
+    }
+    pub fn actor_of(&self, props: Arc<ActorFactory>, name: String) -> ActorRef {
+        let actor_ref = ActorRef::new(props.clone().create(), self.dispatcher.clone());
+        actor_ref
     }
 }
